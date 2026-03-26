cb27fe5625fe20b74310e42d21b0aee13577330b
olica1995 <olicafelix95@gmail.com>
2026-03-26 17:42:28 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kyogatv/app/MainActivity.kt
+++ app/src/main/java/com/kyogatv/app/MainActivity.kt
@@ -1,1 +1,22 @@
+package com.kyogatv.app
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_tv).setOnClickListener {
+            startActivity(Intent(this, TvPlayerActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_radio).setOnClickListener {
+            startActivity(Intent(this, RadioPlayerActivity::class.java))
+        }
+    }
+}
