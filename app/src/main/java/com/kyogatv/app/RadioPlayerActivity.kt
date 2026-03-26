1143141a5677375e2dbe13c16712f25b841a3441
olica1995 <olicafelix95@gmail.com>
2026-03-26 17:52:37 +0300

    Update RadioPlayerActivity.kt

--- app/src/main/java/com/kyogatv/app/RadioPlayerActivity.kt
+++ app/src/main/java/com/kyogatv/app/RadioPlayerActivity.kt
@@ -1,1 +1,36 @@
+package com.kyogatv.app
 
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerView
+
+class RadioPlayerActivity : AppCompatActivity() {
+
+    private lateinit var player: ExoPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.player_layout)
+
+        val playerView = findViewById<PlayerView>(R.id.player_view)
+
+        player = ExoPlayer.Builder(this).build()
+        playerView.player = player
+
+        val mediaItem = MediaItem.fromUri(
+            Uri.parse("https://stream.zeno.fm/4mb1wxhrefhvv.m3u")
+        )
+
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.playWhenReady = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player.release()
+    }
+}
