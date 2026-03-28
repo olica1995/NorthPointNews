5bdda62a8f2e0e4b17b59530bb6a595066612f56
olica1995 <olicafelix95@gmail.com>
2026-03-28 12:23:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from kivy.uix.videoplayer import VideoPlayer
 
 class KyogaTVApp(App):
     def build(self):
-        # Your Kyoga TV Stream
         url = "https://live21.bozztv.com/akamaissh101/ssh101/livetv1995/playlist.m3u8"
-        
-        # Simple, robust player
-        player = VideoPlayer(source=url, state='play', options={'allow_stretch': True})
-        return player
+        return VideoPlayer(source=url, state='play', options={'allow_stretch': True})
 
 if __name__ == "__main__":
     KyogaTVApp().run()
