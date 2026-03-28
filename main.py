4c89ed0e8db3a6bf3b8b6970f122136f8d32c6a9
olica1995 <olicafelix95@gmail.com>
2026-03-28 12:00:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from kivy.app import App
 from kivy.uix.videoplayer import VideoPlayer
+import os
 
 class KyogaTVApp(App):
     def build(self):
-        # Your specific Kyoga TV Stream URL
+        # Kyoga TV Live Stream
         url = "https://live21.bozztv.com/akamaissh101/ssh101/livetv1995/playlist.m3u8"
         
-        # Standard Kivy VideoPlayer
+        # We use the built-in VideoPlayer which is stable on Android
         player = VideoPlayer(
             source=url, 
             state='play', 
